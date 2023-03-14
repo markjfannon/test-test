e191e5d27e0890a92e079834623e2724d7285874
Mark Fannon <markjfannon@Markbook-Pro.local>
2023-03-14 23:04:58 +0000

    Initial commit

---
+++ tests.py
@@ -0,0 +1,9 @@
+import unittest
+
+from my_sum import sum
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        data = [1,2,3]
+        result = sum(data)
+        self.assertEqual(result, 6)
